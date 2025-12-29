eb8707ea67767f96c888fdea61bcd9c90fb92fcb
YoonJaeHwan <jkl0379@naver.com>
2025-12-29 18:00:13 +0900

    못넣은거 추가

---
+++ src/main/java/practice/bytecode/ByteCodeDemo.java
@@ -0,0 +1,18 @@
+package practice.bytecode;
+
+/**
+ * 바이트 코드 실습 예제
+ *
+ * 이 클래스를 컴파일한 후 javap 명령어로 바이트코드 확인
+ */
+public class ByteCodeDemo {
+
+    public static void main(String[] args) {
+        //기본 타입 연산
+        int a = 10;
+        int b = 20;
+        int result = a + b;
+
+        System.out.println("결과 : " + result);
+    }
+}
